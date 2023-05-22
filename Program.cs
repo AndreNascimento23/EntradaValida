6c119fb1d55db1ee34b8e9ce5c1c3d34b79486a8
André Nascimento <andre4.20nascimento@outlook.com>
2023-05-22 13:41:37 -0300

    Atividade Pronta

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int valor;
+
+Console.WriteLine("--- Entrada Válida ---\n");
+
+do
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...: ");
+    valor = Convert.ToInt32(Console.ReadLine());
+}
+while (valor < 0 || valor > 9);
+
+if (valor == 0)
+{
+    Console.WriteLine("\nOperação cancelada.");
+}
+else
+{
+    Console.WriteLine($"Número selecionado = {valor}");
+}
+
+Console.WriteLine("Volte sempre!");
